188eb663dfe99875ad1f1778890e00c859f412f8
Planck1043 <zhao1036713209@gmail.com>
2017-12-22 09:22:07 +0800

    implement Book CRUD

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,65 @@
+class BooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  # def createa
+  #   @book = Book.new(
+  #   :title => params[:book_title],
+  #   :text => params[:book_text]
+  #   )
+  #
+  #   if @book.save
+  #     redirect_to books_path
+  #   else
+  #     render 'new'
+  #   end
+  # end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to books_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to books_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :text)
+  end
+end
---
+++ db/migrate/20171221074537_book.rb
@@ -0,0 +1,10 @@
+class Book < ActiveRecord::Migration[5.0]
+  def change
+    create_table :books do |t|
+      t.string   :title
+      t.text     :text
+
+      t.timestamps
+    end
+  end
+end
